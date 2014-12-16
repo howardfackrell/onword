d27609d8eebe1f354218f40bf6252cc544313919
Howard Fackrell <howard.fackrell@octanner.com>
2014-12-15 23:28:01 -0700

    add joyride, not quite working yet

--- js/main.js
+++ js/main.js
@@ -37,8 +37,49 @@ $(function() {
 		resultArea.append($("<p>You got " +correct + " out of " + total + "</p>"));
 		resultArea.append($("<a href='.'>Play Again?</a>"));
 
-	});
+	});	
+
+  // $("#chooseID").joyride({
+  // 'tipLocation': 'bottom',         // 'top' or 'bottom' in relation to parent
+  // 'nubPosition': 'auto',           // override on a per tooltip bases
+  // 'scrollSpeed': 300,              // Page scrolling speed in ms
+  // 'timer': 2000,                   // 0 = off, all other numbers = time(ms) 
+  // 'startTimerOnClick': true,       // true/false to start timer on first click
+  // 'nextButton': true,              // true/false for next button visibility
+  // 'tipAnimation': 'pop',           // 'pop' or 'fade' in each tip
+  // 'pauseAfter': [],                // array of indexes where to pause the tour after
+  // 'tipAnimationFadeSpeed': 300,    // if 'fade'- speed in ms of transition
+  // 'cookieMonster': true,           // true/false for whether cookies are used
+  // 'cookieName': 'JoyRide',         // choose your own cookie name
+  // 'cookieDomain': false,           // set to false or yoursite.com
+  // 'tipContainer': 'body',            // Where the tip be attached if not inline
+  // 'postRideCallback': noop,       // a method to call once the tour closes
+  // 'postStepCallback': noop    
+  // });
+
 })
+
+$(window).load(function() {
+	$("#chooseID").joyride({
+  'tipLocation': 'bottom',         // 'top' or 'bottom' in relation to parent
+  'nubPosition': 'auto',           // override on a per tooltip bases
+  'scrollSpeed': 300,              // Page scrolling speed in ms
+  'timer': 2000,                   // 0 = off, all other numbers = time(ms) 
+  'startTimerOnClick': true,       // true/false to start timer on first click
+  'nextButton': true,              // true/false for next button visibility
+  'tipAnimation': 'pop',           // 'pop' or 'fade' in each tip
+  'pauseAfter': [],                // array of indexes where to pause the tour after
+  'tipAnimationFadeSpeed': 300,    // if 'fade'- speed in ms of transition
+  'cookieMonster': true,           // true/false for whether cookies are used
+  'cookieName': 'JoyRide',         // choose your own cookie name
+  'cookieDomain': false,           // set to false or yoursite.com
+  'tipContainer': 'body',            // Where the tip be attached if not inline
+  'postRideCallback': noop,       // a method to call once the tour closes
+  'postStepCallback': noop    
+  });
+})
+
+var noop = function() {}
 
 var clearIncorrect = function() {
 	$('.definition').removeClass('incorrect');
